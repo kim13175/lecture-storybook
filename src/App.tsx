ca4a81ef1a84c7fbc286ca5eeb08a9f86621d657
beomjo <dongjun1315@naver.com>
2025-04-09 13:20:59 +0900

    test: tailwindcss test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,16 +17,16 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1 className='font-bold underline text-secondary'>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+        <p className='text-xl font-thin'>
+          Edit <code className='text-2xl'>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-2xl">
         Click on the Vite and React logos to learn more
       </p>
     </>
