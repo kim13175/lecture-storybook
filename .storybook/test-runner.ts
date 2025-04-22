e247192bf2c320aacfe0d45812507da5b7df7aed
beomjo <dongjun1315@naver.com>
2025-04-22 22:48:08 +0900

    feat: test-runner.ts 파일 올바르게 수정

--- .storybook/test-runner.ts
+++ .storybook/test-runner.ts
@@ -1,23 +1,21 @@
 import type { TestRunnerConfig } from '@storybook/test-runner';
+import { injectAxe, checkA11y } from 'axe-playwright';
 
+/*
+ * See https://storybook.js.org/docs/writing-tests/test-runner#test-hook-api
+ * to learn more about the test-runner hooks API.
+ */
 const config: TestRunnerConfig = {
-    // Hook that is executed before the test runner starts running tests
-    setup() {
-        // Add your configuration here.
+    async preVisit(page) {
+        await injectAxe(page);
     },
-    /* Hook to execute before a story is initially visited before being rendered in the browser.
-     * The page argument is the Playwright's page object for the story.
-     * The context argument is a Storybook object containing the story's id, title, and name.
-     */
-    async preVisit(page, context) {
-        // Add your configuration here.
-    },
-    /* Hook to execute after a story is visited and fully rendered.
-     * The page argument is the Playwright's page object for the story
-     * The context argument is a Storybook object containing the story's id, title, and name.
-     */
-    async postVisit(page, context) {
-        // Add your configuration here.
+    async postVisit(page) {
+        await checkA11y(page, 'body', {
+            detailedReport: true,
+            detailedReportOptions: {
+                html: true,
+            },
+        });
     },
 };
 
