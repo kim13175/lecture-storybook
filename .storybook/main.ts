c4853846573b7b19cb8674d827fef6f20eadd911
beomjo <dongjun1315@naver.com>
2025-04-23 16:52:47 +0900

    feat(#login-form): interaction addon 설정

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -10,7 +10,8 @@ const config: StorybookConfig = {
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
     "@storybook/experimental-addon-test",
-    "@storybook/addon-a11y"
+    "@storybook/addon-a11y",
+    "@storybook/addon-interactions",
   ],
   "framework": {
     "name": "@storybook/react-vite",
